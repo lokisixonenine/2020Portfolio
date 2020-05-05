f3a8d51613e027f5acd64423813563a68571a72f
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-05-04 20:26:08 -0700

    Created initial files for new portfolio using React

---
+++ portfolio2020/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+
+
+function App() {
+  return (
+<div className="demo-big-content">
+    <Layout>
+        <Header title="Jaycee's Portfolio" scroll>
+            <Navigation>
+                <Link to="/aboutme">About Jaycee</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>                
+                <Link to="/contact">Contact Jaycee</Link>                                
+            </Navigation>
+        </Header>
+        <Drawer title="Navigation">
+            <Navigation>
+                <Link to="/aboutme">About Jaycee</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>                
+                <Link to="/contact">Contact Jaycee</Link>  
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            < Main />
+
+        </Content>
+    </Layout>
+</div>
+  );
+}
+
+export default App;
