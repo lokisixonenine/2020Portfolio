2f8b3da451b77d688422bd7e50786a4ce298ffb1
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-05-05 18:07:15 -0700

    started work on resume file

--- portfolio2020/src/components/resume.js
+++ portfolio2020/src/components/resume.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
     render() {
         return(
-            <div><h1>Resume</h1></div>
+            <div>
+                <Grid>
+                        <Cell col={3}>
+                            <div style={{textAlign: 'center'}}>
+                            <h2 style={{fontFamily: 'Pacifico'}}>Jaycee Bagtas</h2>                                
+                            <img src="https://lokisixonenine.com/assets/images/jaycee.jpg" alt="jaycee" style={{height: '200px', borderRadius: '50%'}}/>
+                            </div>
+                        </Cell>
+                        <Cell className="resume-right-col" col={9}>Right Side</Cell>
+                </Grid>
+            </div>
         )
     }
 }
